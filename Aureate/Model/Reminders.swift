d07054145597a1dcb26f0db04161ee34fad9b165
KatherinePalevich <katherine.palevich@gmail.com>
2021-11-18 09:50:17 -0800

    Display reminders in To-Do tab from the user's Apple Reminders app.

---
+++ Aureate/Model/Reminders.swift
@@ -0,0 +1,76 @@
+//
+//  Reminders.swift
+//  Aureate
+//
+//  Created by Katherine Palevich on 11/9/21.
+//
+
+import Foundation
+import EventKit
+import Combine
+import SwiftUI
+
+class Reminders: ObservableObject {
+    @Published public var dayReminders : [EKReminder] = []
+    @Published public var weekReminders : [EKReminder] = []
+    private var cancellables = Set<AnyCancellable>()
+    
+    public static let eventStore = EKEventStore()
+    @Published public var date : Date {
+        didSet {
+            populateDayReminders(date: date)
+            //populateWeekReminders(date: date)
+        }
+    }
+
+    public init(date: Date) {
+        self.date = date
+        Reminders.eventStore.requestAccess(to: EKEntityType.reminder){ (granted, error) in
+            if !granted {
+                fatalError("Cannot access events")
+            }
+            self.populateDayReminders(date: self.date)
+            //self.populateWeekReminders(date: self.date)
+        }
+        NotificationCenter.default.publisher(for: .EKEventStoreChanged)
+            .sink{ _ in
+                DispatchQueue.main.async {
+                    Reminders.eventStore.refreshSourcesIfNecessary()
+                    self.populateDayReminders(date: self.date)
+                    //self.populateWeekReminders(date: self.date)
+                }
+            }.store(in: &cancellables)
+    }
+    
+    private func populateDayReminders(date: Date){
+        let predicate: NSPredicate? = Reminders.eventStore.predicateForReminders(in: nil)
+        if let aPredicate = predicate {
+            Reminders.eventStore.fetchReminders(matching: aPredicate, completion: {(_ reminders: [Any]?) -> Void in
+                for reminder: EKReminder in reminders as? [EKReminder] ?? [EKReminder]() {
+                    // Do something for each reminder.
+                    // add all the active reminders
+                    if(!self.dayReminders.contains(reminder) && !reminder.isCompleted){
+                        self.dayReminders.append(reminder)
+                    }
+                }
+            })
+        }
+    }
+    
+//    private func populateWeekReminders(date: Date){
+//        // Get the appropriate calendar.
+//        let calendar = Calendar.current
+//        
+//        let beginningOfWeek = (calendar.nextWeekend(startingAfter: date, direction: .backward)?.end)?.addingTimeInterval(-60 * 60 * 24)
+//        var endOfWeek = (calendar.nextWeekend(startingAfter: date, direction: .forward)?.end)!.addingTimeInterval(-60 * 60 * 24)
+//        if(calendar.isDateInWeekend(date) && calendar.isDateInWeekend(date.addingTimeInterval(60 * 60 * 24))){
+//            endOfWeek = date
+//        }
+//        
+//        
+//        let predicate = Events.eventStore.predicateForEvents(withStart: beginningOfWeek!, end: endOfWeek, calendars: Events.eventStore.calendars(for: EKEntityType.event))
+//        weekReminders = Events.eventStore.events(matching: predicate)
+//        
+//    }
+    
+}
--- Aureate/Views/ToDoView.swift
+++ Aureate/Views/ToDoView.swift
@@ -6,10 +6,40 @@
 //
 
 import SwiftUI
+import EventKit
 
 struct ToDoView: View {
+    @ObservedObject var calendar = Reminders(date: Date())
+    
     var body: some View {
         Text("This is the To-Do Page")
+        RemindersView(calendarReminders: calendar.dayReminders)
+    }
+}
+    
+struct RemindersView : View {
+    var calendarReminders : [EKReminder]
+    @State private var selectedReminder : EKReminder?
+    
+    var body: some View {
+        List {
+            ForEach(calendarReminders, id: \.self) { reminder in
+                ReminderRow(reminder: reminder).onTapGesture {
+                    selectedReminder = reminder
+                }
+            }
+        }
+    }
+}
+
+struct ReminderRow: View {
+    var reminder: EKReminder
+    
+    var body: some View {
+        VStack(alignment:.leading) {
+            Text(reminder.title)
+            //Text("\(reminder.description)")
+        }
     }
 }
 
