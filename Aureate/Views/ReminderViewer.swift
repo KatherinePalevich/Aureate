a96d7fe8870901210e0209acde181fc348c88050
KatherinePalevich <katherine.palevich@gmail.com>
2021-12-09 09:43:59 -0800

    Begin implementation of toolbar on ReminderViewer

--- Aureate/Views/ReminderViewer.swift
+++ Aureate/Views/ReminderViewer.swift
@@ -12,23 +12,45 @@ struct ReminderViewer: View {
     @Binding var reminder : EKReminder
     @State var date = Date()
     
+    @Environment(\.dismiss) var dismiss
+    
+    func setDate() -> Void{
+        date = (reminder.dueDateComponents?.date)!
+    }
+    
     var body: some View {
-        Form {
+        //Form {
             Section {
                 TextField("Title", text: $reminder.title ?? "")
-                TextField("Notes", text: $reminder.notes ?? "")
+                TextField("Notes", text: $reminder.notes ?? "").toolbar {
+                        ToolbarItemGroup(placement: .navigationBarLeading){
+                            Button(action: {
+                                try! Reminders.eventStore.save(reminder, commit: true)
+                                }, label: {
+                                    Image(systemName: "arrowshape.turn.up.right.circle")
+                                })
+                            Button(action: {
+                                dismiss()
+                                }, label: {
+                                    Image(systemName: "x.circle")
+                                })
+                        }
+                }
             }
-//            Section {
-//                DatePicker(
-//                            "Due  Date",
-//                            selection: $date,
-//                            displayedComponents: [.date]
-//                            )
-//                                .frame(maxWidth: .infinity)
-//                                .datePickerStyle(.graphical)
-//            }
-        }.onDisappear {
+            Section {
+                DatePicker(
+                    "Due  Date",
+                    selection: $date,
+                    displayedComponents: [.date]
+                )
+                    .frame(maxWidth: .infinity)
+                    .datePickerStyle(.graphical)
+            }
+        //}
+        .navigationBarTitle( "MAIN" )
+        .onDisappear {
             try! Reminders.eventStore.save(reminder, commit: true)
         }
+        
     }
 }
